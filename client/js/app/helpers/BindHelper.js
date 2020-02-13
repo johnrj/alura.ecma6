09d700682bff2bb1132bcc549ec64adc29f95cc2
Johann Lemmermann <johnrj@gmail.com>
2020-02-12 21:48:39 -0300

    removendo node_modules

--- client/js/app/helpers/BindHelper.js
+++
@@ -1,8 +0,0 @@
-class BindHelper {
-    constructor(model, view, ...props) {
-        let proxy = ProxyFactory.create(model, props, model => view.update(model));
-
-        view.update(model);
-        return proxy;
-    }
-}
--- client/js/app/views/MensagensView.js
+++
@@ -1,9 +0,0 @@
-class MensagensView extends BaseView {
-    constructor(elemento) {
-        super(elemento);
-    }
-
-    template(modelo) {
-        return modelo.texto ? `<p class="alert alert-info">${modelo.texto}</p>` : '';
-    }
-}
